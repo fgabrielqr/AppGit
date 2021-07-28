df246c711b6fc2e1fd6ad1753a9bfd4be2e74d33
fgabrielqr <fgabrielqr@gmail.com>
2021-07-27 22:44:04 -0300

    feat: :fire: Organizando

--- src/Styles/styles.js
+++ src/Styles/styles.js
@@ -1,6 +1,6 @@
 import Theme from '../Styles/Theme';
 export default ({
-    //stilo do componend 
+
     container: {
         marginTop: 10,
 
@@ -11,18 +11,48 @@ export default ({
 
 
     },
-    //estilo do nome  
+
     nickname: {
         paddingLeft: 0,
         fontSize: 17,
         fontFamily: Theme.fonts.rebotoRegular,
     },
-    //estilo do button 
+
     btn: {
         height: 55,
         padding: 15,
         borderLeftWidth: 0.5,
 
     },
+
+    containerInput: {
+        marginTop: 40,
+        marginBottom: 100,
+        width: 350,
+        height: 60,
+        marginLeft: -9,
+        backgroundColor: '#FFFFFF',
+        flexDirection: 'row',
+        alignItems: 'center',
+        borderRadius: 5,
+    },
+
+    input: {
+        flex: 1,
+        height: '100%',
+        paddingLeft: 20,
+        color: Theme.colors.gray,
+        fontSize: 17,
+        backgroundColor: '#FFFFFF',
+        borderRadius: 5,
+    },
+
+    btn1: {
+        height: 55,
+        padding: 15,
+        backgroundColor: '#FFFFFF',
+        borderLeftWidth: 0.5,
+        borderLeftColor: 'gray',
+    }
 
 });
--- src/components/Input.js
+++ src/components/Input.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { View, TextInput, TouchableOpacity } from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
-import Theme from '../Styles/Theme';
+import styles from '../Styles/styles';
 
 export function Input({ placeholder, onPress, onChangeText }) {
 
@@ -12,44 +12,9 @@ export function Input({ placeholder, onPress, onChangeText }) {
                 placeholder={placeholder}
                 onChangeText={onChangeText} />
 
-            <TouchableOpacity style={styles.btn} onPress={onPress}>
+            <TouchableOpacity style={styles.btn1} onPress={onPress}>
                 <AntDesign name="right" size={25} color={'#B2B2B2'} />
             </TouchableOpacity>
         </View>
     )
 }
-
-const styles = StyleSheet.create({
-
-    containerInput: {
-        marginTop: 40,
-        marginBottom: 100,
-        width: 350,
-        height: 60,
-        marginLeft: -9,
-        backgroundColor: '#FFFFFF',
-        flexDirection: 'row',
-        alignItems: 'center',
-        borderRadius: 5,
-    },
-
-    input: {
-        flex: 1,
-        height: '100%',
-        paddingLeft: 20,
-        color: Theme.colors.gray,
-        fontSize: 17,
-        backgroundColor: '#FFFFFF',
-        borderRadius: 5,
-
-
-    },
-    btn: {
-        height: 55,
-        padding: 15,
-        backgroundColor: '#FFFFFF',
-        borderLeftWidth: 0.5,
-        borderLeftColor: 'gray',
-    }
-
-});
